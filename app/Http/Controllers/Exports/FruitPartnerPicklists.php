f191b6eaf2f132471495d1367b2a3f9a0a894305
Simon <sijn82@gmail.com>
2019-07-15 16:48:45 +0100

    arrghhh, hopefully it was just $ this time

--- app/Http/Controllers/Exports/FruitPartnerPicklists.php
+++ app/Http/Controllers/Exports/FruitPartnerPicklists.php
@@ -534,7 +534,7 @@ WithEvents
                     
                     $delivery_entry = new \stdClass;
                     // Added but untested, however I did the same but with milk to the fruitboxes so it should be fine?
-                    $fruitboxes = $this->$fruitboxes[$key];
+                    $fruitboxes = $this->fruitboxes[$key];
                     
                     $additional_fruit = $fruitboxes->where('company_details_id', $milkbox->company_details_id)->where('delivery_day', $milkbox->delivery_day)->first();
                     if (empty($additional_fruit)) {
