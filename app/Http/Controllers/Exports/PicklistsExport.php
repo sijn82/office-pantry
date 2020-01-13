6bf4bfc3f871accf1b8f5e469a4a5ae20020610a
Simon <sijn82@gmail.com>
2020-01-13 16:20:05 +0000

    added two new wednesday routes

--- app/Http/Controllers/Exports/PicklistsExport.php
+++ app/Http/Controllers/Exports/PicklistsExport.php
@@ -96,6 +96,8 @@ WithMultipleSheets
 
         $correctOrderWedThurFri =   [
                                         '3.00 - Weds Float',
+                                        '3.10 - Go West',
+                                        '3.09 - ',
                                         '3.08 - Float 2',
                                         '3.07 - Extra',
                                         '3.06 - Extra Float',
